6a1de551a0a0264751df9739366e9eea111f1539
Bishal Regmi <beeshall@nwk--234-89.nwk.ruw.rutgers.edu>
2017-02-18 20:29:26 -0500

    idk

--- app/src/main/java/com/example/beeshall/openwit/SearchActivity.java
+++ app/src/main/java/com/example/beeshall/openwit/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.beeshall.openwit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,9 @@ public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        final String sid = (String) intent.getExtras().get("sid");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
